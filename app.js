dbd961f9c024f35fd1fa6ffceb9b266c095965d1
Loren Rivers <loren.rivers@hotmail.co.uk>
2024-01-15 12:37:42 +0000

    arrays workshop

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("Hello world!");
+
+const myBlogPosts = [
+  "First Blog Post",
+  "My Second Post",
+  "Third Blog Post",
+  "Fourth Blog Post",
+];
+
+myBlogPosts[4] = "My Fifth Blog Post";
+
+console.log(myBlogPosts[2]);
+
+const favFood = ["cheesy garlic bread", "malteasers", "cheesecake"];
+console.log(favFood);
+
+const favColours = ["orange", "red", "pink"];
+console.log(favColours);
+
+const favNumbers = [4, 7, 9];
+console.log(favNumbers);
